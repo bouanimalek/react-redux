8a5f39729ddc151535aa62833a310f31091b359c
malzak <malzovich@gmail.com>
2021-07-06 11:03:48 +0100

    retreive state from the store using useSelelctor

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import CakeContainer from "./components/CakeContainer";
+import HooksCakeContainer from "./components/HooksCakeContainer";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./App.css";
@@ -8,6 +9,7 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
+        <HooksCakeContainer />
         <CakeContainer />
       </div>
     </Provider>
---
+++ src/components/HooksCakeContainer.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { useSelector } from 'react-redux'
+
+function HooksCakeContainer() {
+
+    const numOfCakes = useSelector(state => state.numOfCakes)
+    
+    return (
+        <div>
+            <h2>Number Of Cakes - {numOfCakes}</h2>
+            <button>Buy Cake</button>
+        </div>
+    )
+}
+
+export default HooksCakeContainer
