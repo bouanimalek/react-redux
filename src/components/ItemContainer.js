2fad54b235b9a307a920d772368e2f363a1f3db2
malzak <malzovich@gmail.com>
2021-07-06 15:21:59 +0100

    mapDispatchToProps with ownProps parameter

--- src/components/ItemContainer.js
+++ src/components/ItemContainer.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { connect } from "react-redux";
+import { buyCake, buyIceCream } from "../redux";
 
 function ItemContainer(props) {
   return (
     <div>
       <h2>Item - {props.item}</h2>
+      <button onClick={props.buyItem}>Buy Items</button>
     </div>
   );
 }
@@ -14,8 +16,17 @@ const mapToStateProps = (state, ownProps) => {
     ? state.cake.numOfCakes
     : state.iceCream.numOfIceCreams;
 
-    return {
-        item: itemState
-    }
+  return {
+    item: itemState,
+  };
 };
-export default connect(mapToStateProps)(ItemContainer);
+
+const mapDispatchToProps = (dispatch, ownProps) => {
+  const dispatchFunction = ownProps.cake
+    ? () => dispatch(buyCake())
+    : () => dispatch(buyIceCream());
+  return {
+      buyItem: dispatchFunction
+  };
+};
+export default connect(mapToStateProps, mapDispatchToProps)(ItemContainer);
