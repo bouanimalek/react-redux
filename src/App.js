1c785d3a97975293fef03c7573c0efa551fa6b0b
malzak <malzovich@gmail.com>
2021-07-06 16:34:15 +0100

    fetch data api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import HooksCakeContainer from "./components/HooksCakeContainer";
 import IceCreamContainer from "./components/IceCreamContainer";
 import NewCakeContainer from "./components/NewCakeContainer";
 import ItemContainer from "./components/ItemContainer";
+import UserContainer from "./components/UserContainer";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import "./App.css";
@@ -12,12 +13,13 @@ function App() {
   return (
     <Provider store={store}>
       <div className="App">
-        <ItemContainer cake/>
+        <UserContainer />
+        {/* <ItemContainer cake/>
         <ItemContainer />
         <HooksCakeContainer />
         <CakeContainer />
         <IceCreamContainer />
-        <NewCakeContainer />
+        <NewCakeContainer /> */}
       </div>
     </Provider>
   );
